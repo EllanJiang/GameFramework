5ec412978c412696fa1119cc33444cc80adb82ed
Jiang Yin <jiangyin@gameframework.cn>
2017-04-28 22:28:20 +0800

    增加日志重定向工具

---
+++ UnityGameFramework/Assets/GameFramework/Scripts/Editor/Misc/LogRedirection.cs
@@ -0,0 +1,89 @@
+﻿//------------------------------------------------------------
+// Game Framework v3.x
+// Copyright © 2013-2017 Jiang Yin. All rights reserved.
+// Homepage: http://gameframework.cn/
+// Feedback: mailto:jiangyin@gameframework.cn
+//------------------------------------------------------------
+
+using System.Reflection;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace UnityGameFramework.Editor
+{
+    /// <summary>
+    /// 日志重定向相关的实用函数。
+    /// </summary>
+    internal static class LogRedirection
+    {
+        private static readonly Regex m_LogRegex = new Regex(@"\(at (.+):(.+)\)");
+
+        [OnOpenAsset(0)]
+        private static bool OnOpenAsset(int instanceId, int line)
+        {
+            string selectedStackTrace = GetSelectedStackTrace();
+            if (string.IsNullOrEmpty(selectedStackTrace))
+            {
+                return false;
+            }
+
+            if (!selectedStackTrace.Contains("UnityGameFramework.Runtime.BaseComponent:LogCallback"))
+            {
+                return false;
+            }
+
+            Match match = m_LogRegex.Match(selectedStackTrace);
+            match = match.NextMatch();
+            if (!match.Success)
+            {
+                return false;
+            }
+
+            InternalEditorUtility.OpenFileAtLineExternal(Application.dataPath + match.Groups[1].Value.Substring(6), int.Parse(match.Groups[2].Value));
+            return true;
+        }
+
+        private static string GetSelectedStackTrace()
+        {
+            Assembly editorWindowAssembly = typeof(EditorWindow).Assembly;
+            if (editorWindowAssembly == null)
+            {
+                return null;
+            }
+
+            System.Type consoleWindowType = editorWindowAssembly.GetType("UnityEditor.ConsoleWindow");
+            if (consoleWindowType == null)
+            {
+                return null;
+            }
+
+            FieldInfo consoleWindowFieldInfo = consoleWindowType.GetField("ms_ConsoleWindow", BindingFlags.Static | BindingFlags.NonPublic);
+            if (consoleWindowFieldInfo == null)
+            {
+                return null;
+            }
+
+            EditorWindow consoleWindow = consoleWindowFieldInfo.GetValue(null) as EditorWindow;
+            if (consoleWindow == null)
+            {
+                return null;
+            }
+
+            if (consoleWindow != EditorWindow.focusedWindow)
+            {
+                return null;
+            }
+
+            FieldInfo activeTextFieldInfo = consoleWindowType.GetField("m_ActiveText", BindingFlags.Instance | BindingFlags.NonPublic);
+            if (activeTextFieldInfo == null)
+            {
+                return null;
+            }
+
+            return (string)activeTextFieldInfo.GetValue(consoleWindow);
+        }
+    }
+}
