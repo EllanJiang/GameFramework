beb9997b4961f90c9d8d624052f8cffdfd269032
Jiang Yin <jiangyin@gameframework.cn>
2017-05-04 12:07:41 +0800

    更新版本号

--- GameFramework/Base/GameFrameworkEntry.cs
+++ GameFramework/Base/GameFrameworkEntry.cs
@@ -15,7 +15,7 @@ namespace GameFramework
     /// </summary>
     public static class GameFrameworkEntry
     {
-        private const string GameFrameworkVersion = "3.0.3";
+        private const string GameFrameworkVersion = "3.0.4";
         private static readonly LinkedList<GameFrameworkModule> s_GameFrameworkModules = new LinkedList<GameFrameworkModule>();
 
         /// <summary>
--- UnityGameFramework/Assets/GameFramework/Scripts/Runtime/Base/GameEntry.cs
+++ UnityGameFramework/Assets/GameFramework/Scripts/Runtime/Base/GameEntry.cs
@@ -21,7 +21,7 @@ namespace UnityGameFramework.Runtime
     /// </summary>
     public static class GameEntry
     {
-        private const string UnityGameFrameworkVersion = "3.0.3";
+        private const string UnityGameFrameworkVersion = "3.0.4";
         private static readonly LinkedList<GameFrameworkComponent> s_GameFrameworkComponents = new LinkedList<GameFrameworkComponent>();
 
         /// <summary>
