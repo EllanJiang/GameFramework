35486f818609148ebd305f1782ee55348a993d15
Jiang Yin <jiangyin@gameframework.cn>
2017-10-31 00:26:18 +0800

    修改程序集接口

--- GameFramework/Utility/Assembly.cs
+++ GameFramework/Utility/Assembly.cs
@@ -17,33 +17,44 @@ namespace GameFramework
         /// </summary>
         public static class Assembly
         {
+            private static readonly System.Reflection.Assembly[] s_Assemblies = null;
             private static readonly Dictionary<string, Type> s_CachedTypes = new Dictionary<string, Type>();
-            private static readonly List<string> s_LoadedAssemblyNames = new List<string>();
 
             static Assembly()
             {
-                System.Reflection.Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
-                foreach (System.Reflection.Assembly assembly in assemblies)
-                {
-                    s_LoadedAssemblyNames.Add(assembly.FullName);
-                }
+                s_Assemblies = AppDomain.CurrentDomain.GetAssemblies();
             }
 
             /// <summary>
-            /// 获取已加载的程序集名称。
+            /// 获取已加载的程序集。
             /// </summary>
-            /// <returns>已加载的程序集名称。</returns>
-            public static string[] GetLoadedAssemblyNames()
+            /// <returns>已加载的程序集。</returns>
+            public static System.Reflection.Assembly[] GetAssemblies()
             {
-                return s_LoadedAssemblyNames.ToArray();
+                return s_Assemblies;
             }
 
             /// <summary>
-            /// 从已加载的程序集中获取类型。
+            /// 获取已加载的程序集中的所有类型。
+            /// </summary>
+            /// <returns>已加载的程序集中的所有类型。</returns>
+            public static Type[] GetTypes()
+            {
+                List<Type> allTypes = new List<Type>();
+                for (int i = 0; i < s_Assemblies.Length; i++)
+                {
+                    allTypes.AddRange(s_Assemblies[i].GetTypes());
+                }
+
+                return allTypes.ToArray();
+            }
+
+            /// <summary>
+            /// 获取已加载的程序集中的指定类型。
             /// </summary>
             /// <param name="typeName">要获取的类型名。</param>
-            /// <returns>获取的类型。</returns>
-            public static Type GetTypeWithinLoadedAssemblies(string typeName)
+            /// <returns>已加载的程序集中的指定类型。</returns>
+            public static Type GetType(string typeName)
             {
                 if (string.IsNullOrEmpty(typeName))
                 {
@@ -63,9 +74,9 @@ namespace GameFramework
                     return type;
                 }
 
-                foreach (string assemblyName in s_LoadedAssemblyNames)
+                foreach (System.Reflection.Assembly assembly in s_Assemblies)
                 {
-                    type = Type.GetType(string.Format("{0}, {1}", typeName, assemblyName));
+                    type = Type.GetType(string.Format("{0}, {1}", typeName, assembly.FullName));
                     if (type != null)
                     {
                         s_CachedTypes.Add(typeName, type);
