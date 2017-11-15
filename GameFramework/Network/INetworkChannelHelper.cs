81003189178476ea65b67ba62f3d18510992ef64
Jiang Yin <jiangyin@gameframework.cn>
2017-11-15 17:15:00 +0800

    网络接口更新

--- GameFramework/Network/INetworkChannelHelper.cs
+++ GameFramework/Network/INetworkChannelHelper.cs
@@ -58,9 +58,10 @@ namespace GameFramework.Network
         /// <summary>
         /// 反序列化消息包。
         /// </summary>
+        /// <param name="packetHeader">消息包头。</param>
         /// <param name="source">要反序列化的来源流。</param>
         /// <param name="customErrorData">用户自定义错误数据。</param>
         /// <returns>反序列化后的消息包。</returns>
-        Packet DeserializePacket(Stream source, out object customErrorData);
+        Packet DeserializePacket(IPacketHeader packetHeader, Stream source, out object customErrorData);
     }
 }
