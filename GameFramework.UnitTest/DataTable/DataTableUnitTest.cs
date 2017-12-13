3cb37d3bc673eeb12af225fe8fca9182d16e9a35
Jiang Yin <jiangyin@gameframework.cn>
2017-12-13 21:01:48 +0800

    增加单元测试工程

---
+++ GameFramework.UnitTest/DataTable/DataTableUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using GameFramework.DataTable;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameFramework.UnitTest.DataTable
+{
+    [TestClass]
+    public class DataTableUnitTest
+    {
+        private IDataTableManager m_DataTableManager = null;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            m_DataTableManager = GameFrameworkEntry.GetModule<IDataTableManager>();
+            Assert.IsNotNull(m_DataTableManager);
+
+            m_DataTableManager.SetResourceManager(null);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            m_DataTableManager = null;
+            GameFrameworkEntry.Shutdown();
+        }
+
+        [TestMethod]
+        public void TestMethod()
+        {
+
+        }
+    }
+}
