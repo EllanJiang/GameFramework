a4d1bd39f6af22f3eddda54af992dc3f6cab0939
Jiang Yin <jiangyin@gameframework.cn>
2017-12-28 18:12:43 +0800

    修改注释

--- GameFramework/Download/DownloadUpdateEventArgs.cs
+++ GameFramework/Download/DownloadUpdateEventArgs.cs
@@ -1,6 +1,6 @@
 ﻿//------------------------------------------------------------
 // Game Framework v3.x
-// Copyright © 2013-2017 Jiang Yin. All rights reserved.
+// Copyright © 2013-2018 Jiang Yin. All rights reserved.
 // Homepage: http://gameframework.cn/
 // Feedback: mailto:jiangyin@gameframework.cn
 //------------------------------------------------------------
