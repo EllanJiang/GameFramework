e0ce6e0ed51766eb55872f020a6e9541f882d703
Jiang Yin <jiangyin@gameframework.cn>
2018-03-07 15:15:01 +0800

    调整配置接口

--- GameFramework/Setting/ISettingHelper.cs
+++ GameFramework/Setting/ISettingHelper.cs
@@ -29,156 +29,156 @@ namespace GameFramework.Setting
         /// <summary>
         /// 检查是否存在指定配置项。
         /// </summary>
-        /// <param name="key">要检查配置项的名称。</param>
+        /// <param name="settingName">要检查配置项的名称。</param>
         /// <returns>指定的配置项是否存在。</returns>
-        bool HasKey(string key);
+        bool HasSetting(string settingName);
 
         /// <summary>
         /// 移除指定配置项。
         /// </summary>
-        /// <param name="key">要移除配置项的名称。</param>
-        void RemoveKey(string key);
+        /// <param name="settingName">要移除配置项的名称。</param>
+        void RemoveSetting(string settingName);
 
         /// <summary>
         /// 清空所有配置项。
         /// </summary>
-        void RemoveAllKeys();
+        void RemoveAllSettings();
 
         /// <summary>
         /// 从指定配置项中读取布尔值。
         /// </summary>
-        /// <param name="key">要获取配置项的名称。</param>
+        /// <param name="settingName">要获取配置项的名称。</param>
         /// <returns>读取的布尔值。</returns>
-        bool GetBool(string key);
+        bool GetBool(string settingName);
 
         /// <summary>
         /// 从指定配置项中读取布尔值。
         /// </summary>
-        /// <param name="key">要获取配置项的名称。</param>
+        /// <param name="settingName">要获取配置项的名称。</param>
         /// <param name="defaultValue">当指定的配置项不存在时，返回此默认值。</param>
         /// <returns>读取的布尔值。</returns>
-        bool GetBool(string key, bool defaultValue);
+        bool GetBool(string settingName, bool defaultValue);
 
         /// <summary>
         /// 向指定配置项写入布尔值。
         /// </summary>
-        /// <param name="key">要写入配置项的名称。</param>
+        /// <param name="settingName">要写入配置项的名称。</param>
         /// <param name="value">要写入的布尔值。</param>
-        void SetBool(string key, bool value);
+        void SetBool(string settingName, bool value);
 
         /// <summary>
         /// 从指定配置项中读取整数值。
         /// </summary>
-        /// <param name="key">要获取配置项的名称。</param>
+        /// <param name="settingName">要获取配置项的名称。</param>
         /// <returns>读取的整数值。</returns>
-        int GetInt(string key);
+        int GetInt(string settingName);
 
         /// <summary>
         /// 从指定配置项中读取整数值。
         /// </summary>
-        /// <param name="key">要获取配置项的名称。</param>
+        /// <param name="settingName">要获取配置项的名称。</param>
         /// <param name="defaultValue">当指定的配置项不存在时，返回此默认值。</param>
         /// <returns>读取的整数值。</returns>
-        int GetInt(string key, int defaultValue);
+        int GetInt(string settingName, int defaultValue);
 
         /// <summary>
         /// 向指定配置项写入整数值。
         /// </summary>
-        /// <param name="key">要写入配置项的名称。</param>
+        /// <param name="settingName">要写入配置项的名称。</param>
         /// <param name="value">要写入的整数值。</param>
-        void SetInt(string key, int value);
+        void SetInt(string settingName, int value);
 
         /// <summary>
         /// 从指定配置项中读取浮点数值。
         /// </summary>
-        /// <param name="key">要获取配置项的名称。</param>
+        /// <param name="settingName">要获取配置项的名称。</param>
         /// <returns>读取的浮点数值。</returns>
-        float GetFloat(string key);
+        float GetFloat(string settingName);
 
         /// <summary>
         /// 从指定配置项中读取浮点数值。
         /// </summary>
-        /// <param name="key">要获取配置项的名称。</param>
+        /// <param name="settingName">要获取配置项的名称。</param>
         /// <param name="defaultValue">当指定的配置项不存在时，返回此默认值。</param>
         /// <returns>读取的浮点数值。</returns>
-        float GetFloat(string key, float defaultValue);
+        float GetFloat(string settingName, float defaultValue);
 
         /// <summary>
         /// 向指定配置项写入浮点数值。
         /// </summary>
-        /// <param name="key">要写入配置项的名称。</param>
+        /// <param name="settingName">要写入配置项的名称。</param>
         /// <param name="value">要写入的浮点数值。</param>
-        void SetFloat(string key, float value);
+        void SetFloat(string settingName, float value);
 
         /// <summary>
         /// 从指定配置项中读取字符串值。
         /// </summary>
-        /// <param name="key">要获取配置项的名称。</param>
+        /// <param name="settingName">要获取配置项的名称。</param>
         /// <returns>读取的字符串值。</returns>
-        string GetString(string key);
+        string GetString(string settingName);
 
         /// <summary>
         /// 从指定配置项中读取字符串值。
         /// </summary>
-        /// <param name="key">要获取配置项的名称。</param>
+        /// <param name="settingName">要获取配置项的名称。</param>
         /// <param name="defaultValue">当指定的配置项不存在时，返回此默认值。</param>
         /// <returns>读取的字符串值。</returns>
-        string GetString(string key, string defaultValue);
+        string GetString(string settingName, string defaultValue);
 
         /// <summary>
         /// 向指定配置项写入字符串值。
         /// </summary>
-        /// <param name="key">要写入配置项的名称。</param>
+        /// <param name="settingName">要写入配置项的名称。</param>
         /// <param name="value">要写入的字符串值。</param>
-        void SetString(string key, string value);
+        void SetString(string settingName, string value);
 
         /// <summary>
         /// 从指定配置项中读取对象。
         /// </summary>
         /// <typeparam name="T">要读取对象的类型。</typeparam>
-        /// <param name="key">要获取配置项的名称。</param>
+        /// <param name="settingName">要获取配置项的名称。</param>
         /// <returns>读取的对象。</returns>
-        T GetObject<T>(string key);
+        T GetObject<T>(string settingName);
 
         /// <summary>
         /// 从指定配置项中读取对象。
         /// </summary>
         /// <param name="objectType">要读取对象的类型。</param>
-        /// <param name="key">要获取配置项的名称。</param>
+        /// <param name="settingName">要获取配置项的名称。</param>
         /// <returns></returns>
-        object GetObject(Type objectType, string key);
+        object GetObject(Type objectType, string settingName);
 
         /// <summary>
         /// 从指定配置项中读取对象。
         /// </summary>
         /// <typeparam name="T">要读取对象的类型。</typeparam>
-        /// <param name="key">要获取配置项的名称。</param>
+        /// <param name="settingName">要获取配置项的名称。</param>
         /// <param name="defaultObj">当指定的配置项不存在时，返回此默认对象。</param>
         /// <returns>读取的对象。</returns>
-        T GetObject<T>(string key, T defaultObj);
+        T GetObject<T>(string settingName, T defaultObj);
 
         /// <summary>
         /// 从指定配置项中读取对象。
         /// </summary>
         /// <param name="objectType">要读取对象的类型。</param>
-        /// <param name="key">要获取配置项的名称。</param>
+        /// <param name="settingName">要获取配置项的名称。</param>
         /// <param name="defaultObj">当指定的配置项不存在时，返回此默认对象。</param>
         /// <returns></returns>
-        object GetObject(Type objectType, string key, object defaultObj);
+        object GetObject(Type objectType, string settingName, object defaultObj);
 
         /// <summary>
         /// 向指定配置项写入对象。
         /// </summary>
         /// <typeparam name="T">要写入对象的类型。</typeparam>
-        /// <param name="key">要写入配置项的名称。</param>
+        /// <param name="settingName">要写入配置项的名称。</param>
         /// <param name="obj">要写入的对象。</param>
-        void SetObject<T>(string key, T obj);
+        void SetObject<T>(string settingName, T obj);
 
         /// <summary>
         /// 向指定配置项写入对象。
         /// </summary>
-        /// <param name="key">要写入配置项的名称。</param>
+        /// <param name="settingName">要写入配置项的名称。</param>
         /// <param name="obj">要写入的对象。</param>
-        void SetObject(string key, object obj);
+        void SetObject(string settingName, object obj);
     }
 }
