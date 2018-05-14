81274adc40d414d569158997e137ebf215f86d60
Jiang Yin <jiangyin@gameframework.cn>
2018-05-14 15:39:03 +0800

    更新版本号

--- GameFramework/Base/GameFrameworkEntry.cs
+++ GameFramework/Base/GameFrameworkEntry.cs
@@ -15,7 +15,7 @@ namespace GameFramework
     /// </summary>
     public static class GameFrameworkEntry
     {
-        private const string GameFrameworkVersion = "3.1.2";
+        private const string GameFrameworkVersion = "3.1.3";
         private static readonly LinkedList<GameFrameworkModule> s_GameFrameworkModules = new LinkedList<GameFrameworkModule>();
 
         /// <summary>
