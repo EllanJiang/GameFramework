1c3031cfdf3eaa8b412d10969354cda153ecdb3e
Jiang Yin <jiangyin@gameframework.cn>
2018-08-09 20:20:26 +0800

    调整日志

--- GameFramework/Utility/Utility.Profiler.cs
+++ GameFramework/Utility/Utility.Profiler.cs
@@ -5,6 +5,8 @@
 // Feedback: mailto:jiangyin@gameframework.cn
 //------------------------------------------------------------
 
+using System.Diagnostics;
+
 namespace GameFramework
 {
     public static partial class Utility
@@ -29,6 +31,8 @@ namespace GameFramework
             /// 开始采样。
             /// </summary>
             /// <param name="name">采样名称。</param>
+            /// <remarks>仅在带有 DEBUG 预编译选项时生效。</remarks>
+            [Conditional("DEBUG")]
             public static void BeginSample(string name)
             {
                 if (s_ProfilerHelper == null)
@@ -42,6 +46,8 @@ namespace GameFramework
             /// <summary>
             /// 结束采样。
             /// </summary>
+            /// <remarks>仅在带有 DEBUG 预编译选项时生效。</remarks>
+            [Conditional("DEBUG")]
             public static void EndSample()
             {
                 if (s_ProfilerHelper == null)
