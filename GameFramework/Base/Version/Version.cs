c0ba718291a1df2bfd63ca46a50f64b9f2844ba1
Jiang Yin <jiangyin@gameframework.cn>
2018-12-01 15:57:26 +0800

    更新版本号

--- GameFramework/Base/Version/Version.cs
+++ GameFramework/Base/Version/Version.cs
@@ -12,7 +12,7 @@ namespace GameFramework
     /// </summary>
     public static partial class Version
     {
-        private const string GameFrameworkVersionString = "3.1.6";
+        private const string GameFrameworkVersionString = "3.1.7";
 
         private static IVersionHelper s_VersionHelper = null;
 
