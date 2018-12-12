d3d076f6dc4ec6bf4d368d62b8efcd3cd1858ff4
Jiang Yin <jiangyin@gameframework.cn>
2018-12-12 10:39:49 +0800

    修正一处退出游戏可能报错的问题

--- GameFramework/Resource/ResourceManager.ResourceLoader.AssetObject.cs
+++ GameFramework/Resource/ResourceManager.ResourceLoader.AssetObject.cs
@@ -121,11 +121,12 @@ namespace GameFramework.Resource
                                 throw new GameFrameworkException(Utility.Text.Format("Asset target '{0}' dependency asset reference count is invalid.", Name));
                             }
                         }
+
+                        m_ResourcePool.Unspawn(m_Resource);
                     }
 
                     m_AssetDependencyCount.Remove(Target);
                     m_ResourceHelper.Release(Target);
-                    m_ResourcePool.Unspawn(m_Resource);
                 }
             }
         }
