4963987b09aa5c2df475f3da8d3bc84fe36c7c3a
Jiang Yin <jiangyin@gameframework.cn>
2019-09-26 16:08:36 +0800

    增加字节长度字符串函数

--- GameFramework/Utility/Utility.Text.cs
+++ GameFramework/Utility/Utility.Text.cs
@@ -132,6 +132,46 @@ namespace GameFramework
                 return string.IsNullOrEmpty(name) ? typeName : Format("{0}.{1}", typeName, name);
             }
 
+            /// <summary>
+            /// 获取字节长度字符串。
+            /// </summary>
+            /// <param name="byteLength">要获取的字节长度值。</param>
+            /// <returns>字节长度字符串。</returns>
+            public static string GetByteLengthString(long byteLength)
+            {
+                if (byteLength < 1024L) // 2 ^ 10
+                {
+                    return Format("{0} B", byteLength.ToString());
+                }
+
+                if (byteLength < 1048576L) // 2 ^ 20
+                {
+                    return Format("{0} KB", (byteLength / 1024f).ToString("F2"));
+                }
+
+                if (byteLength < 1073741824L) // 2 ^ 30
+                {
+                    return Format("{0} MB", (byteLength / 1048576f).ToString("F2"));
+                }
+
+                if (byteLength < 1099511627776L) // 2 ^ 40
+                {
+                    return Format("{0} GB", (byteLength / 1073741824f).ToString("F2"));
+                }
+
+                if (byteLength < 1125899906842624L) // 2 ^ 50
+                {
+                    return Format("{0} TB", (byteLength / 1099511627776f).ToString("F2"));
+                }
+
+                if (byteLength < 1152921504606846976L) // 2 ^ 60
+                {
+                    return Format("{0} PB", (byteLength / 1125899906842624f).ToString("F2"));
+                }
+
+                return Format("{0} EB", (byteLength / 1152921504606846976f).ToString("F2"));
+            }
+
             private static void CheckCachedStringBuilder()
             {
                 if (s_CachedStringBuilder == null)
