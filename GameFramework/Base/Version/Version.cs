17097dedb352d7ddecf3c8b42d820142a79d2f09
Jiang Yin <jiangyin@gameframework.cn>
2019-10-13 22:13:22 +0800

    更新版本号

--- GameFramework/Base/Version/Version.cs
+++ GameFramework/Base/Version/Version.cs
@@ -12,7 +12,7 @@ namespace GameFramework
     /// </summary>
     public static partial class Version
     {
-        private const string GameFrameworkVersionString = "2019.07.20";
+        private const string GameFrameworkVersionString = "2019.10.13";
 
         private static IVersionHelper s_VersionHelper = null;
 
