3b239627b4dcccae7ad5c639fd4c08a4068f345e
Jiang Yin <jiangyin@gameframework.cn>
2020-07-11 16:05:32 +0800

    更新全局配置的加载方式

--- GameFramework/Config/IConfigHelper.cs
+++ GameFramework/Config/IConfigHelper.cs
@@ -25,9 +25,9 @@ namespace GameFramework.Config
         /// 加载全局配置。
         /// </summary>
         /// <param name="configAssetName">全局配置资源名称。</param>
-        /// <param name="configBytes">全局配置二进制数据。</param>
-        /// <param name="startIndex">全局配置二进制数据的起始位置。</param>
-        /// <param name="length">全局配置二进制数据的长度。</param>
+        /// <param name="configBytes">全局配置二进制流。</param>
+        /// <param name="startIndex">全局配置二进制流的起始位置。</param>
+        /// <param name="length">全局配置二进制流的长度。</param>
         /// <param name="userData">用户自定义数据。</param>
         /// <returns>是否加载成功。</returns>
         bool LoadConfig(string configAssetName, byte[] configBytes, int startIndex, int length, object userData);
@@ -43,9 +43,9 @@ namespace GameFramework.Config
         /// <summary>
         /// 解析全局配置。
         /// </summary>
-        /// <param name="configBytes">要解析的全局配置二进制数据。</param>
-        /// <param name="startIndex">全局配置二进制数据的起始位置。</param>
-        /// <param name="length">全局配置二进制数据的长度。</param>
+        /// <param name="configBytes">要解析的全局配置二进制流。</param>
+        /// <param name="startIndex">全局配置二进制流的起始位置。</param>
+        /// <param name="length">全局配置二进制流的长度。</param>
         /// <param name="userData">用户自定义数据。</param>
         /// <returns>是否解析全局配置成功。</returns>
         bool ParseConfig(byte[] configBytes, int startIndex, int length, object userData);
