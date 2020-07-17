16c26847b88fee0b0a0f42cacbe3e3681aa2851d
Jiang Yin <jiangyin@gameframework.cn>
2020-07-17 16:58:37 +0800

    修正文件系统的BUG

--- GameFramework/FileSystem/FileSystem.HeaderData.cs
+++ GameFramework/FileSystem/FileSystem.HeaderData.cs
@@ -52,7 +52,7 @@ namespace GameFramework.FileSystem
             {
                 get
                 {
-                    return m_Header.Length == HeaderLength && m_Header[0] == Header[0] && m_Header[1] == Header[1] && m_Header[2] == m_Header[2] && m_Version == 0 && m_EncryptBytes.Length == EncryptBytesLength
+                    return m_Header.Length == HeaderLength && m_Header[0] == Header[0] && m_Header[1] == Header[1] && m_Header[2] == Header[2] && m_Version == 0 && m_EncryptBytes.Length == EncryptBytesLength
                         && m_MaxFileCount > 0 && m_MaxBlockCount > 0 && m_MaxFileCount <= m_MaxBlockCount && m_BlockCount > 0 && m_BlockCount <= m_MaxBlockCount;
                 }
             }
