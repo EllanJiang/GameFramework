d5413db8a067b78be820878dd08954f84766a050
Jiang Yin <jiangyin@gameframework.cn>
2020-12-31 21:18:26 +0800

    更新版本号

--- GameFramework/Base/Version/Version.cs
+++ GameFramework/Base/Version/Version.cs
@@ -12,7 +12,7 @@ namespace GameFramework
     /// </summary>
     public static partial class Version
     {
-        private const string GameFrameworkVersionString = "2020.11.03";
+        private const string GameFrameworkVersionString = "2020.12.31";
 
         private static IVersionHelper s_VersionHelper = null;
 
